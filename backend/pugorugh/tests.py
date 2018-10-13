b3d98375d565b9edbd88ebb884a192a569f422a3
ykuruta <yasukokurata@gmail.com>
2018-10-13 16:26:29 -0400

    updated tests.py

--- backend/pugorugh/tests.py
+++ backend/pugorugh/tests.py
@@ -3,9 +3,9 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
 from rest_framework.authtoken.models import Token
-#from rest_framework.test import force_authenticate
+from rest_framework.test import APITestCase, APIClient
+
 from .models import Dog, UserDog, UserPref
 from . import views
 
@@ -47,7 +47,7 @@ class TestUserPref(APITestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class TestRetrieveDog(APITestCase):
+class TestUserDog(APITestCase):
     def setUp(self):
         self.user = User.objects.create(
             username='testuser',
@@ -55,7 +55,6 @@ class TestRetrieveDog(APITestCase):
         self.token = Token.objects.create(user=self.user)
         self.client = APIClient()
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-
 
         self.userpref = UserPref.objects.create(
             user=self.user,
@@ -83,38 +82,8 @@ class TestRetrieveDog(APITestCase):
         response = self.client.get('/api/dog/-1/undecided/next/', format='json')
         self.assertEqual(response.status_code, 200)
 
-
-class TestUpdateDog(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create(
-            username='testuser',
-            password='password')
-        self.token = Token.objects.create(user=self.user)
-        self.client = APIClient()
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-
-        self.userpref = UserPref.objects.create(
-            user=self.user,
-            age='b,y,a,s',
-            gender='m,f',
-            size='s,m,l,xl'
-        )
-
-        self.dog = Dog.objects.create(
-            name='test dog',
-            image_filename='dog.pics',
-            breed='husky',
-            gender='f',
-            size='m',
-            age='10'
-        )
-
-        self.userdog = UserDog.objects.create(
-            user=self.user,
-            dog=self.dog,
-            status='l'
-        )
-
     def test_update_dog_status(self):
         response = self.client.put('/api/dog/1/liked/', format='json')
         self.assertEqual(response.status_code, 200)
+
+    
